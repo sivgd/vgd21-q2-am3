264ab7bdb7d403fc77f28766bfff2b36aac52672
tehcakeisalie <1707239@mukilteo.wednet.edu>
2021-01-07 10:58:10 -0800

    Update testing healthbar.unity, Player.meta, and 8 more files...

--- Assets/Jump.cs
+++ Assets/Jump.cs
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -43,3 +44,50 @@ public class Jump : MonoBehaviour
      }
 
    
+=======
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jump : MonoBehaviour
+
+    {
+    public float MovementSpeed = 1;
+    public float JumpForce = 1;
+    private Rigidbody2D _rigidbody;
+    private bool doublejump;
+    private BoxCollider2D box2d;
+
+    private void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody2D>();
+        box2d = transform.GetComponent<BoxCollider2D>();
+    }
+
+   
+    private void Update()
+    {
+
+
+
+        var movement = Input.GetAxis("Horizontal");
+        transform.position += new Vector3(movement, 0, 0) * Time.deltaTime * MovementSpeed;
+
+        if (IsGrounded() && Input.GetKeyDown(KeyCode.Space))
+        {
+            float jumpVelocity = 40f;
+            _rigidbody.velocity = Vector2.up * jumpVelocity;
+        }
+
+
+        }
+        private bool IsGrounded()
+    {
+         RaycastHit2D raycasthit2d = Physics2D.BoxCast(box2d.bounds.center, box2d.bounds.size, 0f, Vector2.down * .1f);
+        Debug.Log(raycasthit2d.collider);
+        return raycasthit2d.collider != null;
+         }
+     }
+
+   
+>>>>>>> main
--- Assets/Trent/Player/Dash.cs
+++ Assets/Trent/Player/Dash.cs
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -71,3 +72,78 @@ public class Dash : MonoBehaviour
         }
     }
 }
+=======
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dash : MonoBehaviour
+{
+    float mx;
+    public float speed = 10f;
+    public float dashDistance = 15f;
+    bool isDashing;
+    float doubleTapTime;
+    KeyCode lastKeyCode;
+    [SerializeField] Rigidbody2D rb;
+    [SerializeField] LayerMask groundlayer;
+    [SerializeField] Transform feet;
+
+    void Start()
+    {
+
+    }
+
+
+    void Update()
+    {
+        if (!isDashing)
+            rb.velocity = new Vector2(mx * speed, rb.velocity.y);
+
+        mx = Input.GetAxis("Horizontal");
+        // Left
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            if (doubleTapTime > Time.time && lastKeyCode == KeyCode.A)
+            {
+                StartCoroutine(Dash(-1f));
+            }
+            else
+            {
+                doubleTapTime = Time.time + 0.5f;
+            }
+
+            lastKeyCode = KeyCode.A;
+        }
+        {
+            // Right
+            if (Input.GetKeyDown(KeyCode.D))
+            {
+                if (doubleTapTime > Time.time && lastKeyCode == KeyCode.D)
+                {
+                    StartCoroutine(Dash(1f));
+                }
+                else
+                {
+                    doubleTapTime = Time.time + 0.5f;
+                }
+
+                lastKeyCode = KeyCode.D;
+            }
+        }
+       
+        IEnumerator Dash(float direction)
+        {
+            isDashing = true;
+            rb.velocity = new Vector2(rb.velocity.x, 0f);
+            rb.AddForce(new Vector2(dashDistance * direction, 0f), ForceMode2D.Impulse);
+            float gravity = rb.gravityScale;
+            rb.gravityScale = 0f;
+            yield return new WaitForSeconds(0.4f);
+            isDashing = false;
+            rb.gravityScale = gravity;
+
+        }
+    }
+}
+>>>>>>> main
