be449381ee123438400bfc7b4653a4ae857b3c11
tehcakeisalie <1707239@mukilteo.wednet.edu>
2021-01-07 10:51:19 -0800

    Merge branch 'main' of https://github.com/sivgd/vgd21-q2-am3
    Conflicts:
    	Assets/Connor/HealthBar/testing healthbar.unity

--- Assets/Trent/Camera/CameraFollow.cs
+++ Assets/Trent/Camera/CameraFollow.cs
@@ -7,7 +7,7 @@ public class CameraFollow : MonoBehaviour
     public GameObject player;
     private Vector3 offset;
 
-    // Start is called before the first frame update
+    
     void Start()
     {
         offset = transform.position - player.transform.position;
@@ -16,7 +16,7 @@ public class CameraFollow : MonoBehaviour
 
     }
 
-    // Update is called once per frame
+    
     void LateUpdate()
     {
         transform.position = player.transform.position + offset;
