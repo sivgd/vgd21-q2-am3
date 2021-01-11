31523cfa85d20fe791c908af6a845029d5e02a1c
tehcakeisalie <1707239@mukilteo.wednet.edu>
2021-01-11 11:14:07 -0800

    added crude, but functional projectile

---
+++ Assets/EnemyHealth.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHealth : MonoBehaviour
+{
+    public int enemyHealth = 100;
+
+
+    public void TakeDamage(int damage)
+    {
+        enemyHealth -= damage;
+
+        if (enemyHealth <= 0)
+        {
+            Die();
+        }
+    }
+
+    void Die()
+    {
+        Destroy(gameObject);
+    }
+
+
+}
