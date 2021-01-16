da6f32ed7b764258571e0fba4bd3aa51d7dc8593
Thomas <1707235@mukilteo.wednet.edu>
2021-01-15 23:53:22 -0800

    Update Backtoogame.cs, Backtoogame.cs.meta, and 28 more files...

---
+++ Assets/Thomas/Backtoogame.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Backtoogame : MonoBehaviour
+{
+    public void Goback()
+    {
+        SceneManager.LoadScene("Menu_Place");
+
+    }
+}
---
+++ Assets/Thomas/script.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class script : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("Demo");
+
+    }
+    public void Quitgame()
+    {
+        Application.Quit();
+    }
+    
+}
+
+
