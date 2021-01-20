84805c7113202cd6d7e9a271f95d82ca4ca333f4
tehcakeisalie <1707239@mukilteo.wednet.edu>
2021-01-20 10:08:03 -0800

    fixed deathbarrier in relation to the healthbar

--- Assets/Demo/Player/Death.cs
+++ Assets/Demo/Player/Death.cs
@@ -7,9 +7,14 @@ public class Death : MonoBehaviour
     public GameObject spawnpoint;
     public GameObject mainPlayer;
     private Rigidbody2D rb2;
+    public HealthBar script2;
 
+    public void MaxHealth()
+    {
+        script2.slider.maxValue = script2.health;
 
-   
+    }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.tag == "Player")
@@ -17,6 +22,12 @@ public class Death : MonoBehaviour
             rb2 = mainPlayer.GetComponent<Rigidbody2D>();
             rb2.velocity = new Vector2(0, 0);
             mainPlayer.transform.localPosition = spawnpoint.transform.localPosition;
+            MaxHealth();
         }
+    }
+
+    private void Update()
+    {
+        script2.slider.value = script2.health;
     }
 }
