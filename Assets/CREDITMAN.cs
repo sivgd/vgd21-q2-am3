772d3d3bb8006f4fcf8b3d04e5ae114b09c514c6
Thomas <1707235@mukilteo.wednet.edu>
2021-01-20 12:41:47 -0800

    Update CREDITMAN.cs, CREDITMAN.cs.meta, and 3 more files...

--- Assets/CREDITMAN.cs
+++ Assets/CREDITMAN.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class scenechanger : MonoBehaviour
+
+
+public class CREDITMAN : MonoBehaviour
 {
     // Start is called before the first frame update
     private void OnTriggerEnter2D(Collider2D collision)
